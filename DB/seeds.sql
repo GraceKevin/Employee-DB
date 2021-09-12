3068760a578abe35dc1d8c10e4d3c0eefff1d268
Kevin Grace <Kevin@kevins-air.home>
2021-09-11 22:05:18 -0400

    added seeds from hw example

--- DB/seeds.sql
+++ DB/seeds.sql
@@ -1,11 +1,29 @@
 INSERT INTO employee
     (first_name, last_name, roles_id, manager_id)
 VALUES
+    ('John', 'Doe', 1, 3),
+    ('Mike', 'Chan', 1, 1),
+    ('Ashley', 'Rodriquez', 2, NULL),
+    ('Kevin', 'Tupik', 2, 3),
+    ('Malia', 'Brown', 3, NULL),
+    ('Sarah', 'Lourd', 3, NULL),
+    ('Tom', 'Allen', 4, 6);
 
 INSERT INTO roles
     (title, salary, department_id)
 VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2), 
+    ('Software Engineer', 120000, 2),
+    ('Accountant', 125000, 3), 
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
 INSERT INTO department
     (description, name)
 VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');`
